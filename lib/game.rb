5a6e2a1383de8e3dbd8e20917bceb6193c6039df
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-02-27 21:49:47 +0530

    Add game end functions

--- lib/game.rb
+++ lib/game.rb
@@ -33,11 +33,38 @@ class Game
       ask_player(@player1)
       @matrix.print_board
       ask_player(@player2)
-      break if player_won?
+      break if game_end?
     end
   end
 
-  def player_won?
-    
+  def row_win?(player)
+    @matrix.matrix.any? { |row| row.all? { |ele| ele == player.mark } }
+  end
+
+  def col_win?(player)
+    @matrix.matrix.transpose.any? { |row| row.all? { |ele| ele == player.mark } }
+  end
+
+  def diagonal_win?(player)
+    @matrix.matrix[0][0] == player.mark && @matrix.matrix[1][1] == player.mark && @matrix.matrix[2][2] == player.mark
+  end
+
+  def player_win?(player)
+    row_win?(player) || col_win?(player) || diagonal_win?(player)
+  end
+
+  def someone_win?
+    if player_win?(@player1)
+      puts "#{@player1.name} wins"
+    elsif player_win?(@player2)
+      puts "#{@player2.name} wins"
+    end
+  end
+
+  def game_end?
+    return true if someone_win?
+    return true if matrix.is_full? && puts('The game is a draw')
+
+    false
   end
 end
